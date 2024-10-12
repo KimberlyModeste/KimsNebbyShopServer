a678128561eb091c268595cde8a9720e54311905
KimberlyModeste <54868542+KimberlyModeste@users.noreply.github.com>
2024-10-12 19:58:54 -0400

    Added exact searches

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -10,6 +10,7 @@ namespace KimsNebbyShopServer.Helpers
         public string? Name { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool isDecsending { get; set; } = false;
+        public bool isExactly { get; set; } = false;
         public int PageNumber { get; set; } = 1;
         public int PageSize { get; set; } = 20;
     }
--- Repository/ItemRepository.cs
+++ Repository/ItemRepository.cs
@@ -14,91 +14,98 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KimsNebbyShopServer.Repository
 {
-    public class ItemRepository : IItemRepository
+  public class ItemRepository : IItemRepository
+  {
+    private readonly ApplicationDBContext _context;
+    public ItemRepository(ApplicationDBContext context)
     {
-        private readonly ApplicationDBContext _context;
-        public ItemRepository(ApplicationDBContext context)
-        {
-            _context = context;
-        }
-
-        //*********************************************************//
-        // PLEASE OH PLEASE FOR THE LOVE OF GOD REMEMBER TO DO THIS
-        // WHEN TRYING TO DO RELATIONSHIPS.
-        // PLEASE REMEMBER TO INCLUDE THE RELATIONSHIP IN THE REPO
-        //*********************************************************//
-        public async Task<List<Item>> GetAllAsync(QueryObject query)
-        {
-            var item = _context.Items.Include(x => x.Images).Include(x => x.Tags).ThenInclude(y =>y.Tag).AsQueryable();
-
-            if(!string.IsNullOrWhiteSpace(query.Name))
-            {
-                item = item.Where(x => x.Name.Contains(query.Name));
-            }
-
-            if(!string.IsNullOrWhiteSpace(query.SortBy))
-            {
-                if(query.SortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
-                {
-                    item = query.isDecsending ? item.OrderByDescending(x => x.Name) : item.OrderBy(x => x.Name);
-                }
-            }
-
-            var skipNumber = (query.PageNumber - 1) * query.PageSize;
-
-
-
-            return await item.Skip(skipNumber).Take(query.PageSize).ToListAsync();
-        }
-
-        public async Task<Item?> GetItemByIdAsync(int id)
-        {
-            return await _context.Items.Include(x => x.Tags).ThenInclude(y =>y.Tag).FirstOrDefaultAsync(z => z.Id == id);
-        }
-        
-        public async Task<Item> CreateAsync(Item itemModel)
-        {
-            await _context.Items.AddAsync(itemModel);
-            await _context.SaveChangesAsync();
-            return itemModel;
-        }
-
-        public async Task<Item?> UpdateAsync(int id, UpdateItemRequestDto itemDto)
-        {
-            var itemModel = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
-            if(itemModel == null)
-            {
-                return null;
-            }
-
-            itemModel.Name = itemDto.Name == "" || itemDto.Name == "string" ? itemModel.Name : itemDto.Name;
-            itemModel.Price = itemDto.Price == 0 ? itemModel.Price : itemDto.Price;
-            itemModel.Description = itemDto.Description;
-            itemModel.OnSale = itemDto.OnSale;
-            itemModel.OnSaleBy = itemDto.OnSaleBy == -1 ? itemModel.OnSaleBy : itemDto.OnSaleBy;
-            itemModel.Amount = itemDto.Amount == -1 ? itemModel.Amount : itemDto.Amount;
-
-            await _context.SaveChangesAsync();
-            return itemModel;
-        }
-
-        public async Task<Item?> DeleteAsync(int id)
-        {
-            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
-            
-            if(item == null)
-            {
-                return null;
-            }
-            _context.Items.Remove(item);
-            await _context.SaveChangesAsync();
-            
-            return item;
-        }
-
-        public Task<bool> ItemExists(int id)
-        {
-            return _context.Items.AnyAsync(x => x.Id==id);
-        }
+      _context = context;
     }
+
+    //*********************************************************//
+    // PLEASE OH PLEASE FOR THE LOVE OF GOD REMEMBER TO DO THIS
+    // WHEN TRYING TO DO RELATIONSHIPS.
+    // PLEASE REMEMBER TO "INCLUDE" THE RELATIONSHIP IN THE REPO
+    //*********************************************************//
+    public async Task<List<Item>> GetAllAsync(QueryObject query)
+    {
+      var item = _context.Items.Include(x => x.Images).Include(x => x.Tags).ThenInclude(y =>y.Tag).AsQueryable();
+      
+      if(!string.IsNullOrWhiteSpace(query.Name))
+      {
+        if(query.isExactly)
+        {
+          item = item.Where(x => x.Name == query.Name);
+        }
+        else
+        {
+          item = item.Where(x => x.Name.Contains(query.Name));
+        }
+      }
+
+      if(!string.IsNullOrWhiteSpace(query.SortBy))
+      {
+          if(query.SortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+          {
+              item = query.isDecsending ? item.OrderByDescending(x => x.Name) : item.OrderBy(x => x.Name);
+          }
+      }
+
+      var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+
+
+      return await item.Skip(skipNumber).Take(query.PageSize).ToListAsync();
+    }
+
+    public async Task<Item?> GetItemByIdAsync(int id)
+    {
+        return await _context.Items.Include(x => x.Tags).ThenInclude(y =>y.Tag).FirstOrDefaultAsync(z => z.Id == id);
+    }
+    
+    public async Task<Item> CreateAsync(Item itemModel)
+    {
+        await _context.Items.AddAsync(itemModel);
+        await _context.SaveChangesAsync();
+        return itemModel;
+    }
+
+    public async Task<Item?> UpdateAsync(int id, UpdateItemRequestDto itemDto)
+    {
+        var itemModel = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+        if(itemModel == null)
+        {
+            return null;
+        }
+
+        itemModel.Name = itemDto.Name == "" || itemDto.Name == "string" ? itemModel.Name : itemDto.Name;
+        itemModel.Price = itemDto.Price == 0 ? itemModel.Price : itemDto.Price;
+        itemModel.Description = itemDto.Description;
+        itemModel.OnSale = itemDto.OnSale;
+        itemModel.OnSaleBy = itemDto.OnSaleBy == -1 ? itemModel.OnSaleBy : itemDto.OnSaleBy;
+        itemModel.Amount = itemDto.Amount == -1 ? itemModel.Amount : itemDto.Amount;
+
+        await _context.SaveChangesAsync();
+        return itemModel;
+    }
+
+    public async Task<Item?> DeleteAsync(int id)
+    {
+        var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+        
+        if(item == null)
+        {
+            return null;
+        }
+        _context.Items.Remove(item);
+        await _context.SaveChangesAsync();
+        
+        return item;
+    }
+
+    public Task<bool> ItemExists(int id)
+    {
+        return _context.Items.AnyAsync(x => x.Id==id);
+    }
+  }
 }
